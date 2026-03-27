6160b08577abb9fd985d4a6e32d6e10722dc0da7
marcoacolla <marcocolla29@gmail.com>
2026-03-26 22:07:29 -0300

    feat(tests): add local tests for sending welcome and embed messages via Discord

--- service/communication/tests.py
+++ service/communication/tests.py
@@ -1,3 +1,42 @@
+import json
+import os
+import sys
 from django.test import TestCase
+from services.discord import DiscordService as discord
 
-# Create your tests here.
+if __package__ in (None, ""):
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def test_send_default_welcome_message():
+    # Simulate sending a welcome message to a new user
+    with open("communication/templates/discord/d_bem_vindo.json") as f:
+        welcome_message = json.load(f)
+    try:
+        discord.send_message(
+            content=welcome_message["content"],
+            webhook_url="https://discord.com/api/webhooks/1486097706198765699/f1PNruEtUuEhQ27x9STjlGApLcumIO02j_hbcqlQETzl4gn6r7EpMMAvkFrSyF5v5Ufb",
+        )
+        result = "Message sent successfully"
+    except Exception as e:
+        result = f"Failed to send message: {e}"
+    print(result)
+
+
+def test_send_embed_message():
+    # Simulate sending an embed message to a new user
+    with open("communication/templates/discord/embed_bem_vindo.json") as f:
+        embed_message = json.load(f)
+    try:
+        discord.send_embed(
+            embed=embed_message,
+            webhook_url="https://discord.com/api/webhooks/1486097706198765699/f1PNruEtUuEhQ27x9STjlGApLcumIO02j_hbcqlQETzl4gn6r7EpMMAvkFrSyF5v5Ufb",
+        )
+        result = "Embed message sent successfully"
+    except Exception as e:
+        result = f"Failed to send embed message: {e}"
+    print(result)
+
+
+test_send_default_welcome_message()
+test_send_embed_message()
