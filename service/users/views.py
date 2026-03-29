12a6668914124b1ae0c2e361818dfaa242727fa6
MarcosMantovani <marcos.vmantovani2004@gmail.com>
2026-03-29 17:56:49 -0300

    refactor(users): apply onboarding guard permission to user viewset

--- service/users/views.py
+++ service/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 
+from authentication.permissions import IsAuthenticatedWithOnboardingGuard
 from users.models import User
 from users.permissions import UserModelPermissions
 from users.serializers import UserSerializer
@@ -8,4 +9,4 @@ from users.serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all().order_by("username")
     serializer_class = UserSerializer
-    permission_classes = [UserModelPermissions]
+    permission_classes = [IsAuthenticatedWithOnboardingGuard, UserModelPermissions]
