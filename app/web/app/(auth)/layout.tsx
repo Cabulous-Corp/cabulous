79436b10e57b0c2d7cae31e35269bf9098352bc3
MarcosMantovani <marcos.vmantovani2004@gmail.com>
2026-08-05 22:20:38 -0300

    feat: modernize login page

--- app/web/app/(auth)/layout.tsx
+++ app/web/app/(auth)/layout.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  // TODO: Criar a estrutura e o layout novo de Login/Auth no futuro
-  return (
-    <div className="divlay flex min-h-screen items-center justify-center bg-background bg-radial-[at_0_100%] from-[#413361]">
-      {children}
-    </div>
-  )
+  return <div className="min-h-svh w-full bg-background">{children}</div>
 }
